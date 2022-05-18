6aad44ac5aff162ad6ddb23d31dfb22df7f54a41
Mahin05 <mahin.islam05@gmail.com>
2022-05-19 04:40:12 +0600

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {v4 as uuidv4} from 'uuid';
+
+const Form = ({input,setInput,todos,setTodos}) => {
+    const onInputChange = event => {
+        setInput(event.target.value);
+    }
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        setTodos([...todos,{id:uuidv4(),title:input,completed:false}]);
+        setInput("");
+    };
+    return (
+        <form onSubmit={onFormSubmit}>
+            <input type="text" placeholder='Enter A TODO' className='task-input' value={input} required onChange={onInputChange} />
+            <button className='button-add' type='Submit'>Add</button>
+        </form>
+    );
+};
+
+export default Form;
